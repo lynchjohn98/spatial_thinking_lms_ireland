d9288cd0fce0983892737fe2c2634ab8bce42664
John Lynch <lynchjohn98@gmail.com>
2024-01-17 14:46:50 -0500

    Newest updates

--- pages/courses/[courseName]/quizzes/post-psvtr.vue
+++ pages/courses/[courseName]/quizzes/post-psvtr.vue
@@ -159,7 +159,6 @@ async function handleSubmit(autoSubmit = false) {
     <NuxtLink :to="`/${basePath}`" class="bg-emerald-500 w-auto px-6 py-4 text-white text-2xl text-center rounded">
       Return to All Quizzes
     </NuxtLink>
-    {{ userStore }}
 </div>
   <div v-else class="quiz-fullpage">
     <form id="quiz-form">
